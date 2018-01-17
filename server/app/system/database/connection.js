e812c80e2863d6a48592f3376886092838e58acb
Austin Haws <ahaws@utah.gov>
2018-01-17 05:47:07 -0700

    Setup mongodb upgrade to 3.0
    It now gets a client instead of a db.

--- server/app/system/database/connection.js
+++ server/app/system/database/connection.js
@@ -1,12 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/football";
+const url = "mongodb://localhost:27017";
 
 const mongo = {db: false};
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function(err, client) {
 	if (err) throw err;
 	console.log("Database connected!");
-	mongo.db = db;
+	mongo.db = client.db('football');
 });
 
 module.exports = mongo;
